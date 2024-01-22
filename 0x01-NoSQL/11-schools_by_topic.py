f2472a2dac24c80f6b1c803f89b583df1242efba
Inialpha <inimfonebong001@gmail.com>
2024-01-22 09:15:01 -0800

    Add shebang

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,4 +1,6 @@
-""" module for a Python function that returns the list of school having a specific topic
+#!/usr/bin/env python3
+""" module for a Python function that returns the
+ist of school having a specific topic
 """
 
 
