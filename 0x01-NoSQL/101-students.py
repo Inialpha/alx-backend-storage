992fc561421337be60dd58f82dbcfb5eb3d733ce
Inialpha <inimfonebong001@gmail.com>
2024-01-23 04:02:17 -0800

    Add task 14

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Python function that returns all students sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """
+    function that returns all students sorted by average score
+    """
+    return mongo_collection.aggregate(
+            [
+                {'$project': {
+                    'name': 1,
+                    'topics': 1,
+                    'averageScore': {'$avg': '$topics.score'}
+                    },
+                 },
+                {'$sort': {'averageScore': -1}}
+                ]
+            )
