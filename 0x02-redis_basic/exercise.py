09332f75891725f81aa630076949c1ff08a50c31
Inialpha <inimfonebong001@gmail.com>
2024-01-24 15:50:21 +0000

    Redis Basics

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+""" module for Cache class """
+import redis
+from uuid import uuid4
+from typing import Any, Callable, List
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ count the number of times a method is called
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> str:
+        """ increment wrapper """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """ keep track of methods inputs and output
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, output)
+        return output
+    return wrapper
+
+def replay(fn: Callable) -> Any:
+    """ print function call details """
+    fn_name = fn.__qualname__
+    input_key = f"{fn_name}:inputs"
+    output_key = f"{fn_name}:outputs"
+    cache = fn.__self__
+    num = cache.get_int(fn_name)
+    output_list = cache._redis.lrange(output_key, 0, -1)
+    input_list = cache._redis.lrange(input_key, 0, -1)
+    print(f"{fn_name} was called {num} times:")
+    for inp, oup in zip(input_list, output_list):
+        print(f"{fn_name}(*({inp},)) -> {oup}")
+
+    
+
+
+class Cache:
+    """
+    the Cache class
+    """
+
+    def __init__(self) -> None:
+        """ initialize a Cache instance """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+        
+        
+    @count_calls
+    @call_history
+    def store(self, data: Any) -> str:
+        """ store method for storing <data> """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) -> Any:
+        data = self._redis.get(key)
+        if key in self._redis:
+            if fn:
+                return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ get a string """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """ get an int """
+        return self.get(key, str)
---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" implement the get_page function """
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+cache = redis.Redis()
+
+def counter(method: Callable) -> Callable:
+    """ set an expiration on a key """
+
+    @wraps(method)
+    def wrapper(*args, **kwargs) -> Callable:
+        """ function wrapper """
+        count_key = f"count:{args}"
+        result_key = f"{args}"
+
+        cache.incr(count_key)
+        result = cache.get(result_key)
+        if result:
+            return result.decode('utf8')
+        result = method(*args)
+        cache.set(count_key, 0)
+        cache.setex(result_key, 10, str(result))
+        return result
+    return wrapper
+
+
+@counter
+def get_page(url: str) -> str:
+    """ get a url respobse """
+
+    return requests.get(url).text
+
+print(get_page('http://slowwly.robertomurray.co.uk'))
